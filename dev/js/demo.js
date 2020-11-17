1dcb1a8b5f3aa5a391f3586e351fe5bf706488e5
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-17 16:28:21 -0500

    ghost movement

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -8,23 +8,16 @@ MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
 
 // const morphTL = gsap.timeline();
 
-const monsterTL = gsap.timeline();
+const logoTL = gsap.timeline();
 
 
-export function drawAnimation(){
+export function logoAnimation(){
 
     // morphTL.to("#blue",{duration: 1, morphSVG:"#star", fill:"#650467"});
     // return morphTL;
 
-    monsterTL.to("#dark-red-1",{duration: 1, morphSVG:"#dark-red-2", fill:"#242424"},"monster")
-            .from("#dark-red-1",{duration: 1,drawSVG:"0"})
-            .to("#light-red-1",{duration: 1, morphSVG:"#light-red-2" , fill:"#6e6e6e"},"monster")
-            .to("#left-eye-1",{duration: 1, morphSVG:"#left-eye-2", fill:"#fff"},"monster")
-            .to("#right-eye-1",{duration: 1, morphSVG:"#right-eye-2", fill:"#fff"},"monster")
-            .to("#mouth-1",{duration: 1, morphSVG:"#mouth-2", fill:"#fff"},"monster")
-            .to("#leg-1",{duration: 1, morphSVG:"#horn-1"},"monster")
-            .to("#leg-2",{duration: 1, morphSVG:"#horn-2"},"monster")
-            .to("#shadow-1",{duration: 1, morphSVG:"#shadow-2"},"monster")
-            //.to("#dark-red-1",{duration: 1, morphSVG:"#dark-red-1"});
-    return monsterTL;
+    logoTL.to("#open-top",{duration: 2, y: -500 },"open")
+            .to("#open-bottom",{duration: 2, y: 500 },"open")
+            .to("#blob",{duration: 3, morphSVG:"#ghost-fill"});
+    return logoTL;
 }
