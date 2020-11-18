8b84b4896226310b81f48ca88e5aca4dbf868b21
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-18 13:04:46 -0500

    timing

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -16,10 +16,10 @@ export function logoAnimation(){
     // morphTL.to("#blue",{duration: 1, morphSVG:"#star", fill:"#650467"});
     // return morphTL;
 
-    logoTL.to("#open-top",{duration: 2, y: -500 },"open")
-            .to("#open-bottom",{duration: 2, y: 500 },"open")
+    logoTL.to("#open-top",{duration: 1, y: -500 },"open")
+            .to("#open-bottom",{duration: 1, y: 500 },"open")
             .from("#blob", {duration:1, scale:0})
-            .to("#blob", {duration:2, scale:2, x:150, y: -20})
-            .to("#blob",{duration: 3, morphSVG:"#ghost-fill"});
+            .to("#blob", {duration:1, scale:1, x:150, y: -20})
+            .to("#blob",{duration: 2, morphSVG:"#ghost-fill"});
     return logoTL;
 }
